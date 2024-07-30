fa00df28ea97c2b07e625bbb7e0abec6a3d4ccc8
sanjaysanju673 <sanjuvadde1111@gmail.com>
2024-07-30 16:58:47 +0530

    [Sanjay] Add:Program to caluculate the daily employee wage

--- Employeewagecompution.py
+++ Employeewagecompution.py
@@ -1,6 +1,25 @@
+'''
+@Author: v sanjay kumar
+@Date: 2024-07-30 03:00:30
+@Last Modified by: v sanjay kumar
+@Last Modified time: 2024-07-30 03:00:30
+@Title : 
+'''
 import random
 
+
 def CheckAttendance():
+    
+    """
+    Description:
+    Function that check the attendence for employee.
+
+    Parameters:
+    - None.
+    Returns:
+    -None.
+    """
+
     attendnce =random.choice((0,1))
     print(attendnce)
 
@@ -10,7 +29,18 @@ def CheckAttendance():
         print(f"the employe is absent")
     
 
+
+
 def Dailywage():
+    
+    '''Description:
+    Function that caluculate the daily employe wage.
+
+    Parameters:
+    - None.
+    Returns:
+    -None.
+    '''
     dailywage = 20
     hours = 8
     employeedailywage = dailywage *hours
